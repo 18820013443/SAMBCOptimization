e516fd31e760e718c7788bf83fae22171ea8a642
kk <331479961@qq.com>
2023-08-04 18:35:32 +0800

    Add function ReadXlsToDf in ExcelUtils.py

--- ExcelUtils.py
+++ ExcelUtils.py
@@ -1,20 +1,23 @@
 # import win32com.client as win32
 import os
+import sys
+import traceback
+
+import pandas as pd
 
 
 class ExcelUtils:
-    
+
     def __init__(self):
         self.app = win32.DispatchEx('Excel.Application')
         self.app.Application.DisplayAlerts = False
         self.app.Application.ScreenUpdating = False
         self.app.Application.Visible = False
-    
 
     def ConvertXlsToXlsx(self, filePath):
         try:
             fileName, extension = os.path.splitext(os.path.basename(filePath))
-            fileName = '%s.xlsx'%fileName
+            fileName = '%s.xlsx' % fileName
             dirName = os.path.dirname(filePath)
             newFilePath = os.path.join(dirName, fileName)
             wk = self.app.Workbooks.Open(filePath, False, False)
@@ -22,8 +25,27 @@ class ExcelUtils:
             # print(wk.Sheets(sheetName).Range("A1").Value)
             wk.Close()
             self.app.Quit()
-        except:
+        except Exception as e:
             self.app.Quit()
+            strE = traceback.format_exc()
+            raise Exception(strE)
+
+    def ReadXlsToDf(self, filePath):
+        try:
+            fileName, extension = os.path.splitext(os.path.basename(filePath))
+            fileName = '%s.xlsx' % fileName
+            dirName = os.path.dirname(filePath)
+            newFilePath = os.path.join(dirName, fileName)
+            wk = self.app.Workbooks.Open(filePath, False, False)
+            wk.UsedRange.Copy()
+            wk.Close()
+            self.app.Quit()
+            df = pd.read_clipboard()
+            return df
+        except Exception as e:
+            self.app.Quit()
+            strE = traceback.format_exc()
+            raise Exception(strE)
 
 
 if __name__ == '__main__':
